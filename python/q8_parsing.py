852bd3a5a0a779e875abef1b3a9a3b4045dd2bfb
alexandercameronh <alexandercameronh@gmail.com>
2017-04-04 12:20:20 -0700

    finished Parsing section

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,55 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  4 11:13:57 2017
+
+@author: alexanderhughes
+"""
+
+import csv
+
+
+
+with open('/Users/alexanderhughes/football.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+
+print(type(readCSV))
+
+
+goals_made=[]
+goals_allowed=[]
+newList=[]
+teams=[]
+
+f =open('/Users/alexanderhughes/football.csv')
+csv_f=csv.reader(f)
+
+for row in csv_f:
+    goals_made.append(row[5])
+    goals_allowed.append(row[6])
+    teams.append(row[0])
+    
+del goals_made[0]
+del goals_allowed[0]
+del teams[0]
+
+
+goals_made=list(map(int, goals_made))
+goals_allowed=list(map(int, goals_allowed))
+for i in range(0, len(goals_made)):
+    diff=goals_made[i]-goals_allowed[i]
+    newList.append(diff)
+
+newList=list(map(abs, newList))
+
+min(newList)
+correspondIndex=newList.index(min(newList))
+
+print("The team with the smallest difference in Goals Scored vs Goals Allowed is: {}".format(teams[correspondIndex]))
+
+
+
+
